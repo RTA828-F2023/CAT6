d4747c2c99d320372864e2a6b9134c668c03d226
Xing Wu <41498567+silverlobster@users.noreply.github.com>
2023-09-26 12:43:19 -0400

    removed timer boolean debug value

--- Assets/Scripts/Waves/WavesTimer.cs
+++ Assets/Scripts/Waves/WavesTimer.cs
@@ -26,10 +26,12 @@ public class WavesTimer : MonoBehaviour {
 
         if (TimerOn) {
             if (TimeLeft > 0) {
+                TimerText.enabled = true;
                 TimeLeft -= Time.deltaTime;
                 updateTimer(TimeLeft);
             }
             else {
+                TimerText.enabled = false;
                 TimeLeft = 0;
                 TimerOn = false;
                 updateTimer(TimeLeft);
@@ -46,7 +48,7 @@ public class WavesTimer : MonoBehaviour {
         float minutes = Mathf.FloorToInt(currentTime / 60);
         float seconds = Mathf.FloorToInt(currentTime % 60);
 
-        TimerText.text = "Wave starts in: " + string.Format("{0:00}:{1:00}", minutes, seconds) + TimerOn.ToString();
+        TimerText.text = "Wave starts in: " + string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
     public static implicit operator WavesTimer(bool v)
