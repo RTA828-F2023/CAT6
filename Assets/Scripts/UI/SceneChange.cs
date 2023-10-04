59fd4a95a0c3506624ba78ab19754f571ffb7b96
Racc000n <v009abd@gmail.com>
2023-10-04 11:33:44 -0400

    FINAL Pause UI!!
    Done with Pause UI!! if you spot any bugs pls tell me

--- Assets/Scripts/UI/SceneChange.cs
+++ Assets/Scripts/UI/SceneChange.cs
@@ -1,3 +1,9 @@
+//Author: Victoria Ouryvski 
+//Project Name: CAT6
+//File Name: UIControlller.cs
+//Creation Date: Oct 2, 2023
+//Modified Date: Oct 10, 2023
+//Description: Script to change scene
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +13,7 @@ public class SceneChange : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    //get scene that will be changed to
     public string sceneName; 
     void Start()
     {
@@ -19,8 +26,12 @@ public class SceneChange : MonoBehaviour
         
     }
 
+    //pre: none
+    //post: none
+    //desc: changes scnee to main menu
     public void ChangeToMenu()
     {
+        //change scene to given scene
         SceneManager.LoadScene(sceneName);
     }
 }
