c8c553588fc3fbe5de57656a2bbecf7a5aeb0451
Duong Vu <vu.t.duong01@gmail.com>
2023-10-31 11:07:27 -0400

    Revert bomb projectile damage mechanic

--- Assets/Scripts/Projectile/Bomb.cs
+++ Assets/Scripts/Projectile/Bomb.cs
@@ -20,17 +20,25 @@ public class Bomb : Projectile
         // If hit an enemy then deal damage to it
         if (other.transform.CompareTag("Enemy"))
         {
-            var enemiesHits = Physics2D.OverlapCircleAll(transform.position, radius, LayerMask.GetMask("Enemies"));
-            foreach (var enemyHit in enemiesHits)
-            {
-                var direction = (enemyHit.transform.position - transform.position).normalized;
-                var enemy = enemyHit.GetComponent<Enemy>();
+            var enemy = other.transform.GetComponent<Enemy>();
+            enemy.TakeDamage(damage);
 
-                enemy.TakeDamage(damage);
-                enemyHit.GetComponent<Rigidbody2D>().AddForce(direction * blastForce, ForceMode2D.Impulse);
+            var direction = (enemy.transform.position - transform.position).normalized;
+            enemy.GetComponent<Rigidbody2D>().AddForce(direction * blastForce, ForceMode2D.Impulse);
+            playerOwner.UpdateScore(enemy.GetScore());
 
-                playerOwner.UpdateScore(enemy.GetScore());
-            }
+            // TODO: Implement blast radius
+            // var enemiesHits = Physics2D.OverlapCircleAll(transform.position, radius, LayerMask.GetMask("Enemies"));
+            // foreach (var enemyHit in enemiesHits)
+            // {
+            //     var direction = (enemyHit.transform.position - transform.position).normalized;
+            //     var enemy = enemyHit.GetComponent<Enemy>();
+
+            //     enemy.TakeDamage(damage);
+            //     enemyHit.GetComponent<Rigidbody2D>().AddForce(direction * blastForce, ForceMode2D.Impulse);
+
+            //     playerOwner.UpdateScore(enemy.GetScore());
+            // }
 
             //PointSystemController.UpdatePlayerScore(player.type, enemy.GetScore());
             CameraShaker.Instance.Shake(CameraShakeMode.Light);
