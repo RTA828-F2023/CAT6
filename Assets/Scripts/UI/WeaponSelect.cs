f2a6451f9364611ae0823a1fdc14e789cd663c70
Racc000n <v009abd@gmail.com>
2023-11-06 15:57:31 -0500

    Made Simple WeaponSelectUI

---
+++ Assets/Scripts/UI/WeaponSelect.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class WeaponSelect : MonoBehaviour
+{
+    private InputManager _inputManager;
+    public TextMeshProUGUI next;
+
+    public GameObject WeaponSelectMenu;
+    public GameObject P1W1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //WeaponSelectMenu.SetActive(true);
+        //P1W1.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
